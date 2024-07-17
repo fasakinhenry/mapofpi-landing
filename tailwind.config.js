63bdee7b356936e71c84de39ee4e05f191ec005c
Fasakin Henry <fasakinhenry@gmail.com>
2024-07-17 18:19:15 +0100

    styles: add custom colors in the tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-bg': '#121212',
+        'light-gradient': 'linear-gradient(90deg, #FF6FD8 0%, #3813C2 100%)',
+        'dark-gradient': 'linear-gradient(90deg, #3813C2 0%, #121212 100%)',
+      },
+    },
   },
   plugins: [],
-}
+};
