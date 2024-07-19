0a31a74b6ec195b6d725aeef69435be807bf37fe
Fasakin Henry <fasakinhenry@gmail.com>
2024-07-19 16:08:16 +0100

    feat: update the contents and look of the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav className='flex justify-between items-center py-6 px-10 bg-transparent absolute w-full'>
+    <nav className='flex justify-between items-center py-6 px-10 absolute w-full z-10'>
       <img src='/logo.png' alt='Logo' className='w-8 h-8' />
       <ul className='flex space-x-8 text-gray-800'>
         <li className='font-GT text-lg'>
