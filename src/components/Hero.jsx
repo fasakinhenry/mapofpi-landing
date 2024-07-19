4e53348eb062482a45308012bce3c04863407ab2
Fasakin Henry <fasakinhenry@gmail.com>
2024-07-19 23:06:41 +0100

    feat: update the hero Section of the page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,25 +1,34 @@
 import React from 'react';
+import '../index.css';
 
 const Hero = () => {
   return (
-    <section className='flex items-center justify-center min-h-screen bg-white'>
-      <div className='text-center px-4'>
-        <h1 className='text-5xl font-Clutch font-bold mb-4'>
+    <section className='relative flex items-center justify-center min-h-screen bg-white'>
+      <div className='absolute inset-0 bg-cover h-[90vh] min-h-[83vh] opacity-30 scale-[1.2] bg-hero-image z-0'></div>
+      <div className='text-center px-4 max-w-screen-lg mx-auto backdrop-blur-lg backdrop-opacity-50 rounded-lg p-6'>
+        <h1 className='text-[75px] font-Clutch font-bold mb-10 leading-tight'>
           Get <span className='italic font-Curl'>noticed</span> with perfectly
-          tailored resumes.
+          tailored <span className='italic font-Curl'>resumes</span>.
         </h1>
-        <p className='text-lg text-gray-600 mb-6'>
+        <p className='text-lg text-gray-600 mb-10 mx-20 font-GS'>
           In this brutal job market, we are leveling the playing field and
           boosting your chances of landing your dream job.
         </p>
-        <button className='relative flex items-center justify-center px-6 py-3 text-lg font-medium text-white bg-black rounded-full hover:bg-white hover:text-black shadow-md'>
-          Get Started
-          <img
-            src='/stickman.gif'
-            alt='Running Stickman'
-            className='absolute w-6 h-6 ml-2 animate-running'
-          />
-        </button>
+        <div className='flex justify-center'>
+          <button className='relative flex items-center justify-center px-6 py-3 text-lg font-medium text-white bg-black rounded-full hover:bg-white hover:text-black shadow-md transition-all duration-300'>
+            Get Started
+            <img
+              src='/stickman.png'
+              alt='Running Stickman'
+              className='w-6 h-6 ml-2 inline-block hover:hidden'
+            />
+            <img
+              src='/stickman.gif'
+              alt='Running Stickman'
+              className='w-6 h-6 ml-2 hidden hover:inline-block animate-running'
+            />
+          </button>
+        </div>
       </div>
     </section>
   );
