66b443f59516099d4321e1f67c3ac1f8c96d31a6
Fasakin Henry <fasakinhenry@gmail.com>
2024-07-19 23:07:14 +0100

    feat: update the Navbar to fit design requirements

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,23 +2,25 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav className='flex justify-between items-center py-6 px-10 absolute w-full z-10'>
-      <img src='/logo.png' alt='Logo' className='w-8 h-8' />
-      <ul className='flex space-x-8 text-gray-800'>
-        <li className='font-GT text-lg'>
-          <a href='#'>Home</a>
-        </li>
-        <li className='font-GT text-lg'>
-          <a href='#'>Features</a>
-        </li>
-        <li className='font-GT text-lg'>
-          <a href='#'>Developers</a>
-        </li>
-        <li className='font-GT text-lg'>
-          <a href='#'>Community</a>
-        </li>
-      </ul>
-    </nav>
+    <div className='absolute top-0 left-1/2 transform -translate-x-1/2 mt-5 bg-white rounded-full shadow-md px-3 py-1 z-20'>
+      <nav className='flex justify-between items-center'>
+        <img src='/logo.png' alt='Logo' className='w-8 h-7 mx-3' />
+        <ul className='flex space-x-1 text-gray-800 py-0.5'>
+          <li className='font-GT text-[15px] px-3 py-1 font-extrabold rounded-full bg-[#e7e7e7]'>
+            <a href='#'>Home</a>
+          </li>
+          <li className='font-GT text-[15px] px-3 py-1 font-extrabold rounded-full hover:bg-[#e7e7e7]'>
+            <a href='#'>Features</a>
+          </li>
+          <li className='font-GT text-[15px] px-3 py-1 font-extrabold rounded-full hover:bg-[#e7e7e7]'>
+            <a href='#'>Developers</a>
+          </li>
+          <li className='font-GT text-[15px] px-3 py-1 font-extrabold rounded-full hover:bg-[#e7e7e7]'>
+            <a href='#'>Community</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
