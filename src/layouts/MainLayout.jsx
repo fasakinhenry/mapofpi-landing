a97c1ca254c569c7ad795625465c5535aba88357
Fasakin Henry <fasakinhenry@gmail.com>
2024-07-19 23:08:08 +0100

    feat: add the layout for the landing page

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,7 +3,7 @@ import Navbar from '../components/Navbar';
 
 const MainLayout = ({ children }) => {
   return (
-    <div>
+    <div className='relative bg-[ff6d6d] overflow-hidden'>
       <Navbar />
       <main>{children}</main>
     </div>
