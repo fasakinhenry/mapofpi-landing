fac192a573ba5f9e164c4cc752c0ce33855da15e
Fasakin Henry <fasakinhenry@gmail.com>
2024-07-20 10:43:40 +0100

    styles: make the navbar full width on mobiles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   };
 
   return (
-    <div className='fixed top-0 left-1/2 transform -translate-x-1/2 mt-5 bg-white rounded-full shadow-md px-3 py-1 z-20'>
+    <div className='fixed top-0 w-full md:w-auto mx-4 md:mx-0 md:left-1/2 md:transform md:-translate-x-1/2 mt-5 bg-white rounded-full shadow-md px-3 py-1 z-20'>
       <nav className='flex justify-between items-center w-full'>
         <img src='/logo.png' alt='Logo' className='w-8 h-7 mx-3' />
         <ul className='hidden md:flex space-x-1 text-gray-800 py-0.5 list-none'>
@@ -43,7 +43,7 @@ const Navbar = () => {
         </button>
       </nav>
       {isOpen && (
-        <ul className='absolute top-14 left-1/2 transform -translate-x-1/2 bg-white rounded-md shadow-lg py-2 w-56 list-none'>
+        <ul className='absolute top-14 left-0 w-full bg-white rounded-md shadow-lg py-2 list-none px-4'>
           <li className='font-GT text-[15px] px-4 py-2 font-extrabold rounded-full hover:bg-[#e7e7e7]'>
             <a href='#'>Home</a>
           </li>
